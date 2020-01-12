ee6f3e08242bc037200c81f39b8b567cdfc87ecf
Johan van Soest <info@johanvansoest.nl>
2020-01-12 13:31:15 +0100

    Updated exception handling and exit after 5 consecutive errors

--- run.py
+++ run.py
@@ -18,20 +18,29 @@ serial_reader = SerialReader(
 )
 
 abort = False
+errorCount = 0
 
 while not abort:
-    for telegram in serial_reader.read_as_object():
-        timestamp = int(round(time.time() * 1000000000))
-        for attribute in varsToCollect:
-            value = str(getattr(telegram, attribute).value)
-            unit = str(getattr(telegram, attribute).unit)
-            #print(attribute + " | " + value)
+    try:
+        for telegram in serial_reader.read_as_object():
+            timestamp = int(round(time.time() * 1000000000))
+            for attribute in varsToCollect:
+                value = str(getattr(telegram, attribute).value)
+                unit = str(getattr(telegram, attribute).unit)
+                #print(attribute + " | " + value)
 
-            resultPost = requests.post("http://localhost:8086/write?db=meterstanden",
-                            data="standen,variable=%s,unit=%s value=%s %s" % (attribute, unit, value, timestamp),
-                            headers={"Content-Type": "application/x-www-form-urlencoded"})
-            #print(str(resultPost.text))
-        break
+                resultPost = requests.post("http://localhost:8086/write?db=meterstanden",
+                                data="standen,variable=%s,unit=%s value=%s %s" % (attribute, unit, value, timestamp),
+                                headers={"Content-Type": "application/x-www-form-urlencoded"})
+                #print(str(resultPost.text))
+            errorCount = 0
+            break
+    except:
+        print("Something went wrong when reading meter output")
+        errorCount += 1
     
     # next second of retrieval is always sleepTime + 1 therefore removing 1 second
     time.sleep(sleepTime - 1)
+
+    if errorCount > 5:
+        abort = True
