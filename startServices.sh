9d7d4caa2b648fc7e64366b1a3d5ad8e6ad12bf3
Johan van Soest <info@johanvansoest.nl>
2020-08-09 07:37:51 +0000

    Updates service start script with option to launch single containers

--- startServices.sh
+++ startServices.sh
@@ -1,3 +1,6 @@
+
+if [ -z "$1" ] || [ "$1" -eq "influxdb" ]
+then
 docker volume create influxdb_storage
 docker run -d \
     --name influxdb \
@@ -5,7 +8,10 @@ docker run -d \
     -p 8086:8086 \
     -v influxdb_storage:/var/lib/influxdb \
     influxdb
+fi
 
+if [ -z "$1" ] || [ "$1" -eq "grafana" ]
+then
 docker volume create grafana_storage
 docker run -d \
     --name grafana \
@@ -14,3 +20,20 @@ docker run -d \
     -v grafana_storage:/var/lib/grafana \
     --link influxdb \
     grafana/grafana
+fi
+
+if [ -z "$1" ] || [ "$1" -eq "dsmr_reader" ]
+then
+
+if [[ "$(docker images -q dsmr_reader 2> /dev/null)" == "" ]]; then
+    cd reader_service && docker build -t dsmr_reader ./
+fi
+
+docker run -d \
+    --name dsmr_reader \
+    --restart unless-stopped \
+    --link influxdb \
+    -e INFLUXDB_API_URL=http://influxdb:8086 \
+    -e INFLUXDB_DB_NAME=meterstanden \
+    dsmr_reader
+fi
